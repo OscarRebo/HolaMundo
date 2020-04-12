cddb0cfdf1c627b29cf38f3b21fa7958eb4599a7
OscarRebo <63542849+OscarRebo@users.noreply.github.com>
2020-04-12 20:04:36 +0200

    Create Tabla.java

---
+++ Tabla.java
@@ -0,0 +1,10 @@
+public class Tabla
+{
+    public static void main(String args[])
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            System.out.print("10 * "+ i + " = " + 10 * i + "\n");
+        }
+    }
+}
